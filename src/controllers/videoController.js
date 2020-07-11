62f1d92b5432bfd9fd6ff3488d28442838faed46
Tony Park <tonypark0403@gmail.com>
2020-07-11 00:20:36 -0400

    added search form and checked query string

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,5 +1,8 @@
 export const home = (req, res) => res.render('home', { pageTitle: 'Home', potato: 1245 });
-export const search = (req, res) => res.render('search', { pageTitle: 'Search' });
+export const search = (req, res) => {
+  const { term: searchingBy } = req.query;
+  res.render('search', { pageTitle: 'Search', searchingBy });
+};
 export const upload = (req, res) => res.render('upload', { pageTitle: 'Upload' });
 export const videoDetail = (req, res) => res.render('videoDetail', { pageTitle: 'Video Detail' });
 export const editVideo = (req, res) => res.render('editVideo', { pageTitle: 'Edit Video' });
