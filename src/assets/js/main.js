88880e666a3f52c084499b6f94635780cace6433
Tony Park <tonypark0403@gmail.com>
2020-08-23 02:32:10 -0400

    implemented play button and action with js

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -1,6 +1,3 @@
 import '../scss/styles.scss';
 
-// eslint-disable-next-line import/prefer-default-export
-export const webpackTestFunction = async () => {
-  console.log('webpack test');
-};
+import './videoPlayer';
